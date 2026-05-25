6cf998f3a893bc219151d1e67df27538c8ee7ac2
3057hub <3057hub@users.noreply.github.com>
2026-05-26 03:37:55 +0800

    fix: use Gradle wrapper 8.5 + fix dependencyResolutionManagement syntax

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,8 +6,9 @@ pluginManagement {
         maven { url "https://chaquo.com/maven" }
     }
 }
-dependencyResolution {
-    @Suppress("UnstableApiUsage")
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
